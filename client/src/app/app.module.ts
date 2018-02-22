27de5cfcc77db3760f28e832dcda51a13ad7f910
Sungjae Park <park1637@morris.umn.edu>
2018-02-22 14:42:27 -0600

    Updated todos.component.ts file with using users.component.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,8 +8,10 @@ import {HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {UserComponent} from './users/user.component';
+import {TodoComponent} from "./todos/todo.component";
 import {UserListComponent} from './users/user-list.component';
 import {UserListService} from './users/user-list.service';
+import {TodoListService} from "./todos/todo-list.service";
 import {Routing} from './app.routes';
 import {APP_BASE_HREF} from '@angular/common';
 
@@ -28,11 +30,16 @@ import {AddUserComponent} from './users/add-user.component';
         AppComponent,
         HomeComponent,
         UserListComponent,
+        TodoComponent,
         UserComponent,
         AddUserComponent
     ],
     providers: [
         UserListService,
+        {provide: APP_BASE_HREF, useValue: '/'},
+        {provide: MATERIAL_COMPATIBILITY_MODE, useValue: true},
+
+        TodoListService,
         {provide: APP_BASE_HREF, useValue: '/'},
         {provide: MATERIAL_COMPATIBILITY_MODE, useValue: true}
     ],
